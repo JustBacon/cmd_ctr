b5c25807a402b11afc5bddfbbd852cac257b91a4
Christian Buco <christianbuco17@gmail.com>
2016-12-14 09:22:32 -1000

    update

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function color(){
+	console.log("should turn red");
+	document.getElementById('box').style.backgroundColor = "red"
+}
+function color1(){
+	document.getElementById('box').style.backgroundColor = "blue"
+}
